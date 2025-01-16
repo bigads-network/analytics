7beb541e3f36b8536c499e621094d1795dc58b0d
drake <deepak.kumar@thewasserstoff.com>
2025-01-16 13:24:16 +0530

    added validators

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,10 +4,10 @@ import controllers from "../controllers";
 import { authenticateUser, validateRequest } from "../middleware";
 import validators from "../validators";
 
-router.post('/registerUser',controllers.User.registerUser);
-router.post('/registerGame',authenticateUser ,controllers.User.registerGame);
-router.get('/transactions',controllers.User.transactions)
-router.post('/sendEvents',authenticateUser,controllers.User.sendEvents);
+router.post('/registerUser',validateRequest(validators.user.registerUser),controllers.User.registerUser);
+router.post('/registerGame',validateRequest(validators.user.registerGame),authenticateUser,controllers.User.registerGame);
+router.post('/sendEvents',validateRequest(validators.user.sendEvent),authenticateUser,controllers.User.sendEvents);
+router.get('/transactions',validateRequest(validators.user.transactions),controllers.User.transactions)
 router.get('/count',controllers.User.count)
 router.get('/games',controllers.User.games)
 // router.get('/data' , controllers.User.allData);
